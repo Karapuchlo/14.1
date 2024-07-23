f02ebe7e94704f30b24eb82ad0d62cc70872ee91
grayd <sharin@you-x.ru>
2024-07-23 20:02:44 +0300

    исправил ошибку в представлении категорий

--- category.py
+++ category.py
@@ -2,15 +2,17 @@ class Category:
     _total_categories = 0
     _total_products = 0
 
-    def __init__(self, name, description):
+    def __init__(self, name, description, products=None):
         self.name = name
         self.description = description
-        self.products = []
+        self.products = products or []
         Category._total_categories += 1
+        Category._total_products += len(self.products)
 
     def add_product(self, product):
         self.products.append(product)
         Category._total_products += 1
+
 
     @property
     def total_categories(self):
